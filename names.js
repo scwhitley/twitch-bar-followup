0f3945ff9583b6d4cd9bf161558d96f1382a4c18
Stephen Whitley <stephenwhitley07@gmail.com>
2025-10-28 18:10:30 -0400

    Add arrays for male, female, neutral names and last names

--- names.js
+++ names.js
@@ -1,1 +1,17 @@
+// data/names.js
+export const maleFirst = [
+  "Kael","Darius","Ren","Aric","Jaxon","Tarin","Marek","Silas","Corvin","Lucan"
+];
+
+export const femaleFirst = [
+  "Rhea","Nyra","Selene","Asha","Lyra","Kara","Vera","Tamsin","Mira","Zara"
+];
+
+export const neutralFirst = [
+  "Rowan","Avery","Kai","Salem","Arden","Riven","Sage","Ember","Quinn","Noa"
+];
+
+export const lastNames = [
+  "Vorren","Kade","Thorne","Blackstar","Voss","Draven","Morrow","Kestrel","Hale","Verris"
+];
 
