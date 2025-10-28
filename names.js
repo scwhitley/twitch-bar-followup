12a2a6a5d6a465346dbdb9e914ae7f410f8e5202
Stephen Whitley <stephenwhitley07@gmail.com>
2025-10-28 18:51:16 -0400

    Refactor names.js for clarity and organization
    Updated formatting and added comments to the names array.

--- names.js
+++ names.js
@@ -1,17 +1,35 @@
-// data/names.js
+// names.js
 export const maleFirst = [
-  "Kael","Darius","Ren","Aric","Jaxon","Tarin","Marek","Silas","Corvin","Lucan"
+  // === Core list ===
+  "Kael", "Darius", "Ren", "Aric", "Jaxon", "Tarin", "Marek", "Silas", "Corvin", "Lucan",
+  // === Expanded ===
+  "Zarek", "Ronan", "Kellan", "Drake", "Evren", "Galen", "Cassian", "Theron", "Lior", "Rhett",
+  "Auron", "Cyrus", "Bastian", "Malric", "Elias", "Nero", "Gavric", "Luthor", "Damien", "Soren",
+  "Orin", "Tavian", "Quint", "Jareth", "Ryder", "Dane", "Korran", "Asher", "Talon", "Riven",
+  "Kade", "Varek", "Doran", "Finnian", "Grayson", "Holden", "Coren", "Jace", "Eldric", "Rowan",
+  "Leoric", "Sylas", "Nikolai", "Thorne", "Valor", "Magnus", "Voss", "Rurik", "Varyn", "Zane"
 ];
 
 export const femaleFirst = [
-  "Rhea","Nyra","Selene","Asha","Lyra","Kara","Vera","Tamsin","Mira","Zara"
+  "Rhea", "Nyra", "Selene", "Asha", "Lyra", "Kara", "Vera", "Tamsin", "Mira", "Zara",
+  "Seren", "Isolde", "Celia", "Alara", "Corinne", "Maeve", "Liora", "Thalia", "Elara", "Sabine",
+  "Cassia", "Nara", "Evelis", "Sera", "Aveline", "Iria", "Kallista", "Renna", "Fiora", "Ophel",
+  "Delia", "Astrid", "Ryn", "Caelia", "Morwen", "Selara", "Vivra", "Neria", "Anara", "Tess",
+  "Leona", "Dahlia", "Sorrel", "Emera", "Faye", "Nolana", "Ciri", "Ysara", "Keira", "Maren"
 ];
 
 export const neutralFirst = [
-  "Rowan","Avery","Kai","Salem","Arden","Riven","Sage","Ember","Quinn","Noa"
+  "Rowan", "Avery", "Kai", "Salem", "Arden", "Riven", "Sage", "Ember", "Quinn", "Noa",
+  "Ellis", "Finley", "Jules", "Ash", "Pax", "Milan", "Reese", "Taylor", "Indigo", "Sky",
+  "Alex", "River", "Phoenix", "Sasha", "Marin", "Briar", "Tarian", "Lior", "Wren", "Cam",
+  "Devon", "Laken", "Robin", "Soren", "Micah", "Ari", "Storm", "Valen", "Dune", "Rey",
+  "Zen", "Aven", "Remy", "Dakota", "Marlow", "Torrin", "Nova", "Hale", "Sable", "Echo"
 ];
 
 export const lastNames = [
-  "Vorren","Kade","Thorne","Blackstar","Voss","Draven","Morrow","Kestrel","Hale","Verris"
+  "Vorren", "Kade", "Thorne", "Blackstar", "Voss", "Draven", "Morrow", "Kestrel", "Hale", "Verris",
+  "Rook", "Vale", "Grayson", "Stone", "Cross", "Ashen", "Corvus", "Vyre", "Crowe", "Fenn",
+  "Nash", "Darrow", "Locke", "Kael", "Varin", "Dusk", "Orion", "Slade", "Mercer", "Riven",
+  "Storme", "Vex", "Harrow", "Grimm", "Aldane", "Wraith", "Korr", "Drax", "Hale", "Crest",
+  "Raines", "Talon", "Merrin", "Shade", "Rowe", "Varyn", "Korran", "Black", "Myre", "Tane"
 ];
-
