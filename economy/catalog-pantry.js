d9c82442a343376f53d951ddf40b2c55c836ab6b
Stephen Whitley <stephenwhitley07@gmail.com>
2025-10-29 21:39:28 -0400

    Create catalog-pantry.js

---
+++ economy/catalog-pantry.js
@@ -0,0 +1,10 @@
+export const PANTRY_ITEMS = [
+  { name: "Bread Loaf", price: 10, tag: "food" },
+  { name: "Milk Carton", price: 12, tag: "food" },
+  { name: "Egg Dozen", price: 15, tag: "food" },
+  { name: "Energy Drink", price: 25, tag: "drink" },
+  { name: "Ramen Kit", price: 18, tag: "cookable" },
+  { name: "Coffee Beans", price: 22, tag: "beverage" },
+  { name: "Jerky Pack", price: 14, tag: "snack" },
+  { name: "Focus Brew", price: 50, tag: "buff" },
+];
