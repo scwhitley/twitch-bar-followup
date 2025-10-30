ac3e95965249e4c4c3b898300bf64527739c3608
Stephen Whitley <stephenwhitley07@gmail.com>
2025-10-29 21:38:42 -0400

    Create inventory-commands.js

---
+++ economy/inventory-commands.js
@@ -0,0 +1,18 @@
+import { listInventory } from "./econ-core.js";
+import { EmbedBuilder } from "discord.js";
+
+export async function onMessageCreate(msg) {
+  if (msg.author.bot) return;
+  if (!msg.content.startsWith("!inventory")) return;
+  const inv = await listInventory(msg.author.id);
+  if (!Object.keys(inv).length)
+    return msg.reply("Your inventory is empty. Go shopping!");
+  const desc = Object.entries(inv)
+    .map(([k, v]) => `• **${k}** ×${v}`)
+    .join("\n");
+  const e = new EmbedBuilder()
+    .setTitle(`${msg.author.username}'s Inventory`)
+    .setDescription(desc)
+    .setColor("Blue");
+  await msg.channel.send({ embeds: [e] });
+}
