ebce845d81576939df37b7437be43291c2ef6c08
Stephen Whitley <stephenwhitley07@gmail.com>
2025-11-12 11:43:24 -0500

    Update index.js

--- factions/index.js
+++ factions/index.js
@@ -1,31 +1,36 @@
 // /factions/index.js
 import express from "express";
 
-// ---- Discord chat handlers you expose from factions (optional) ----
-export { onMessageCreate as onEloMsg } from "./elo/elo-commands.js";
+// Convert
+import { convertRouter }   from "./elo/convert-routes.js";
 
-// ---- Routers (HTTP endpoints) ----
-import { convertRouter }    from "./elo/convert-routes.js";
-import { rallyRouter }      from "./actions/rally-route.js";
-import { meditateRouter }   from "./actions/meditate-route.js";
-import { seetheRouter }     from "./actions/seethe-route.js";
-import { eventRouter }      from "./actions/event-route.js";
-import { invasionRouter }   from "./actions/invasion-routes.js";
-import { duelRouter }       from "./actions/duel-route.js";
+// Actions
+import { rallyRouter }     from "./actions/rally-route.js";
+import { meditateRouter }  from "./actions/meditate-route.js";
+import { seetheRouter }    from "./actions/seethe-route.js";
+import { eventRouter }     from "./actions/event-route.js";
+import { invasionRouter }  from "./actions/invasion-routes.js";
+import { duelRouter }      from "./actions/duel-route.js";
 
+// Force trial (legacy HTTP flow)
 import { forceTrialRouter } from "./trial/force-trial-routes.js";
 
-// ---- Build one combined router and export it ----
+// Re-export any Discord message handlers you still use (not routers)
+export { onMessageCreate as onEloMsg } from "./elo/elo-commands.js";
+
 const router = express.Router();
 
-// Mount sub-routers. Order only matters if paths overlap.
+// Mount sub-routers (order only matters if paths overlap)
+if (eloRouter)        router.use(eloRouter);
 router.use(convertRouter);
+
 router.use(rallyRouter);
 router.use(meditateRouter);
 router.use(seetheRouter);
 router.use(eventRouter);
 router.use(invasionRouter);
 router.use(duelRouter);
+
 router.use(forceTrialRouter);
 
 export { router as factionsRouter };
