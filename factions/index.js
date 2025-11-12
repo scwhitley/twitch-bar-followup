aff0a135f8c42f3c60dbbfbfc3221871c87e5af6
Stephen Whitley <stephenwhitley07@gmail.com>
2025-11-12 12:36:36 -0500

    Refactor factionsRouter to use express.Router

--- factions/index.js
+++ factions/index.js
@@ -1,36 +1,37 @@
 // /factions/index.js
 import express from "express";
 
-// Convert
-import { convertRouter }   from "./elo/convert-routes.js";
+// ---- HTTP Routers (each file must export a Router with this name) ----
+import { convertRouter }    from "./elo/convert-routes.js";
+// If you convert elo-commands.js into an HTTP router later, import it here:
+// import { eloRouter }     from "./elo/elo-commands.js";
 
-// Actions
-import { rallyRouter }     from "./actions/rally-route.js";
-import { meditateRouter }  from "./actions/meditate-route.js";
-import { seetheRouter }    from "./actions/seethe-route.js";
-import { eventRouter }     from "./actions/event-route.js";
-import { invasionRouter }  from "./actions/invasion-routes.js";
-import { duelRouter }      from "./actions/duel-route.js";
+import { rallyRouter }      from "./actions/rally-route.js";
+import { meditateRouter }   from "./actions/meditate-route.js";
+import { seetheRouter }     from "./actions/seethe-route.js";
+import { eventRouter }      from "./actions/event-route.js";
+import { invasionRouter }   from "./actions/invasion-routes.js";
+import { duelRouter }       from "./actions/duel-route.js";
 
-// Force trial (legacy HTTP flow)
 import { forceTrialRouter } from "./trial/force-trial-routes.js";
 
-// Re-export any Discord message handlers you still use (not routers)
+// ---- Optional: Discord message handlers (not HTTP) ----
 export { onMessageCreate as onEloMsg } from "./elo/elo-commands.js";
 
-const router = express.Router();
+// ---- Aggregate router ----
+export const factionsRouter = express.Router();
 
 // Mount sub-routers (order only matters if paths overlap)
-if (eloRouter)        router.use(eloRouter);
-router.use(convertRouter);
+factionsRouter.use(convertRouter);
 
-router.use(rallyRouter);
-router.use(meditateRouter);
-router.use(seetheRouter);
-router.use(eventRouter);
-router.use(invasionRouter);
-router.use(duelRouter);
+// If/when you have an eloRouter HTTP router, uncomment the line below
+// factionsRouter.use(eloRouter);
 
-router.use(forceTrialRouter);
+factionsRouter.use(rallyRouter);
+factionsRouter.use(meditateRouter);
+factionsRouter.use(seetheRouter);
+factionsRouter.use(eventRouter);
+factionsRouter.use(invasionRouter);
+factionsRouter.use(duelRouter);
 
-export { router as factionsRouter };
+factionsRouter.use(forceTrialRouter);
