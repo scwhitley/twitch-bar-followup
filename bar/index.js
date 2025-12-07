e7e4fb4dd5062456069b270f6abe6069d001f5b1
Stephen Whitley <stephenwhitley07@gmail.com>
2025-12-07 18:59:06 -0500

    Add registerDrinkRoutes function to index.js
    Added a function to register drink-related API routes.

--- bar/index.js
+++ bar/index.js
@@ -1,6 +1,17 @@
 // /bar/index.js
 // Facade for all drink-related logic
 
+import router from "./drink-routes.js";
 export { onMessageCreate } from "./drink-routes.js";
+
+// Facade to register all bar-related API routes
+export function registerDrinkRoutes(app) {
+  app.use(router);
+}
+
+
+
+
+
 
 
