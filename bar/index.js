b7418437d19c020253dcf19f45ae7ddcb2a1094c
Stephen Whitley <stephenwhitley07@gmail.com>
2025-12-07 19:05:06 -0500

    Refactor index.js to streamline imports and exports

--- bar/index.js
+++ bar/index.js
@@ -1,13 +1,16 @@
 // /bar/index.js
 // Facade for all drink-related logic
 
-import router from "./drink-routes.js";
+import { router, onMessageCreate } from "./drink-routes.js";
 export { onMessageCreate } from "./drink-routes.js";
 
 // Facade to register all bar-related API routes
 export function registerDrinkRoutes(app) {
   app.use(router);
 }
+
+export { onMessageCreate };
+
 
 
 
