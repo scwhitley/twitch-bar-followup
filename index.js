273bdb4e591c0c2ed9c9a15478a83c83f57f2b85
Stephen Whitley <stephenwhitley07@gmail.com>
2025-12-09 00:50:18 -0500

    Remove duplicate factionsRouter middleware
    Removed duplicate factionsRouter middleware usage.

--- index.js
+++ index.js
@@ -105,7 +105,6 @@ app.use(express.static("public")); // if you serve /public
 app.use("/factions", factionsRouter);
 // If you do this, update any Wizebot/overlay URLs accordingly.
 app.locals.redis = redis;
-app.use(factionsRouter);
 
 registerDrinkRoutes(app);
 
