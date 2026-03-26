064cc5d691eed10dcc9b96a5bc7cb27ac4802be1
Stephen Whitley <stephenwhitley07@gmail.com>
2026-03-26 17:34:18 -0400

    deleting old discord code

--- bar/index.js
+++
@@ -1,9 +0,0 @@
-// /bar/index.js
-import { router, onMessageCreate } from "./drink-routes.js";
-
-export function registerDrinkRoutes(app) {
-  // Mount all drink routes under /drinks
-  app.use("/drinks", router);
-}
-
-export { onMessageCreate };
